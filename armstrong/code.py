99fd57ee687efc65276fbd264ae9a3c443521176
Tatsuya <pruthvikanth007@gmail.com>
2023-01-11 11:20:55 +0530

    fix

--- armstrong/code.py
+++ armstrong/code.py
@@ -1,9 +1,10 @@
 num = int(input("Enter the number to check: "))
 temp = num
+sum = 0
 while temp != 0:
     rem = temp % 10
-    sum = sum + rem * rem * rem
-    temp = temp / 10
+    sum += rem ** 3
+    temp = temp // 10
 
 if sum == num:
     print(f"{num} is a armstrong number!")
