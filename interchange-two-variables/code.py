8740a4c92da1f5ac862f388a969670d1480f53a6
Tatsuya <pruthvikanth007@gmail.com>
2023-01-11 10:05:55 +0530

    changes

--- interchange-two-variables/code.py
+++ interchange-two-variables/code.py
@@ -1,5 +1,8 @@
 first = int(input("Enter the first number: "))
 second = int(input("Enter the second number: "))
-first, second = second, first
-print("The first numer is now:", first, "\n"
+first += second
+second = first-second
+first = first-second
+
+print("The first number is now:", first, "\n"
       "The second number is now:", second)
